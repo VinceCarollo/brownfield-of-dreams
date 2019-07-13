6a14fdb8e419b339f204cae11df60fbaa0633a33
Vince Carollo <vincecarollo@gmail.com>
2019-07-13 00:54:24 -0600

    Fix user dashboard facade spec after last refactor

--- spec/facades/user_dashboard_facade_spec.rb
+++ spec/facades/user_dashboard_facade_spec.rb
@@ -9,11 +9,13 @@ RSpec.describe UserDashboardFacade do
   end
 
   it 'can tell if user is current friend' do
-    user2 = create(:user)
-    user3 = create(:user)
+    user2 = create(:user, github_username: 'Vince')
+    visitor1 = Person.new({login: user2.github_username})
+    visitor2 = Person.new({login: 'Jason'})
+
     @user1.friendships << user2
 
-    expect(@facade.user_friend?(user2)).to be true
-    expect(@facade.user_friend?(user3)).to be false
+    expect(@facade.user_friend?(visitor1)).to be true
+    expect(@facade.user_friend?(visitor2)).to be false
   end
 end
