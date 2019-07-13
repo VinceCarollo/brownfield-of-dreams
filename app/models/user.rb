5094ff0cb695f9a45bfad45d4638e63c71b8c7b7
Ryan Miller <ryanmillergm@gmail.com>
2019-07-13 14:57:45 -0600

    Fixed confirmation email status active

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,10 +15,11 @@ class User < ApplicationRecord
   def email_activate
     self.email_confirmed = true
     self.confirm_token = nil
+    save
   end
 
   validates :email, uniqueness: true, presence: true
-  validates_presence_of :password
+  validates_presence_of :password, on: :create
   validates_presence_of :first_name
   enum role: %i[default admin]
   has_secure_password
